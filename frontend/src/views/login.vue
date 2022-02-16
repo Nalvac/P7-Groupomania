dcd1ba3086913053d6a315320a8d051bf33ff3bf
Nalvac <vacna1997@gmail.com>
2022-02-16 14:02:18 +0100

    Login_SignUp-Page

---
+++ frontend/src/views/login.vue
@@ -0,0 +1,41 @@
+<template>
+  <main class="main main--connect">
+    <form class="w-75 align-items-center form-block d-flex m-auto shadow rounded">
+      <div
+        class="form-block--left d-flex flex-column justify-content-center block-demi-container p-3 text-right align-self-stretch"
+      >
+        <img class="logo align-self-end" src="../assets/icon.svg" alt="Logo Groupomania" />
+        <p>
+          <small>
+            Vous n'avez pas encore de compte,
+            <router-link class="redirection-singup" to="/signUp">enregistrez-vous</router-link>
+          </small>
+        </p>
+      </div>
+      <div class="block-demi-container p-3">
+        <div class="form-group">
+          <label for="inputUsername">Username</label>
+          <input type="text" class="form-control" id="inputUsername" />
+        </div>
+        <div class="form-group">
+          <label for="inputPassword">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="inputPassword"
+          />
+        </div>
+        <button type="submit" class="btn btn-primary" >Submit</button>
+      </div>
+    </form>
+  </main>
+</template>
+
+<script>
+ 
+
+</script>
+
+<style lang="scss">
+    @import "./src/scss/global.scss";
+</style>
